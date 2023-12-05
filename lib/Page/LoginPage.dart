96c7782f11e711df4346c18b0af726000100e03a
SYSTEMS\wahaj.khalil <wahaj.khalil@systemsltd.com>
2023-12-05 19:39:26 +0500

    code pushed

--- lib/Page/LoginPage.dart
+++ lib/Page/LoginPage.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:core'; // Add this line to import the core library for the RegExp class
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+  final RegExp phoneRegex = RegExp(r'^[0-9]{10}$');
+
+  // Track the validation status of each field
+  bool isPhoneNumberValid = true;
+  bool isPasswordValid = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-           color: Color(0xFF357AFF),
+          color: Color(0xFF357AFF),
           child: ListView(
             children: [
               Stack(
@@ -15,8 +29,9 @@ class LoginScreen extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(top: 80),
                     child: Image.asset(
-                      'assets/images/Pattern.png', // Path to your second image asset
-                     ),
+                      'assets/images/Pattern.png',
+                      width: double.infinity,
+                    ),
                   ),
                   Column(
                     children: [
@@ -24,122 +39,148 @@ class LoginScreen extends StatelessWidget {
                         child: Padding(
                           padding: const EdgeInsets.only(top: 100.0),
                           child: Image.asset(
-                            'assets/images/Logo_App.png', // Path to your image asset
-                            height: 150, // Adjust the height according to your needs
-                            width: 150, // Fit within the available space
+                            'assets/images/Logo_App.png',
+                            height: 150,
+                            width: 150,
                           ),
                         ),
                       ),
-                      Container(
-                        height: 700,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(80.0),
-                            topRight: Radius.circular(80.0),
+                      Padding(
+                        padding: const EdgeInsets.only(top:18.0),
+                        child: Container(
+                          height: 700,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(40.0),
+                              topRight: Radius.circular(40.0),
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(20.0),
-                              child: Padding(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 10, left: 20),
+                                    child: Text(
+                                      'Sign In',
+                                      style: TextStyle(
+                                        fontSize: 24.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 0.0),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20, right: 20),
+                                child: _buildTextField(
+                                  'Phone Number | User ID ',
+                                  Icons.phone,
+                                  phoneNumberController,
+                                  isPassword: false,
+                                  isValid: isPhoneNumberValid,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 20, right: 20),
+                                child: _buildTextField(
+                                  'Password',
+                                  Icons.lock,
+                                  passwordController,
+                                  isPassword: true,
+                                  isValid: isPasswordValid,
+                                ),
+                              ),
+                              const SizedBox(height: 20.0),
+                              Container(
+                                height: 56,
+                                width: double.infinity,
+                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      // Validate each field and update the validation status
+                                      isPhoneNumberValid = phoneRegex.hasMatch(phoneNumberController.text) &&
+                                          phoneNumberController.text.isNotEmpty;
+                                      isPasswordValid = passwordController.text.isNotEmpty;
+                                    });
+
+                                    if (_isInputValid()) {
+                                      // Add your sign-in logic here
+                                    } else {
+                                      // Show an error message or handle empty/invalid input
+                                    }
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: Colors.orange,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      side: BorderSide(color: Colors.orange),
+                                    ),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      'Sign In',
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Padding(
                                 padding: const EdgeInsets.all(8.0),
-                                child: Padding(
-                                  padding: const EdgeInsets.only(top: 20, left: 20),
-                                  child: Text(
-                                    'Sign In',
-                                    style: TextStyle(
-                                      fontSize: 24.0,
-                                      fontWeight: FontWeight.bold,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    // Add your forgot password logic here
+                                  },
+                                  child: Center(
+                                    child: Text(
+                                      'Forgot Password?',
+                                      style: TextStyle(
+                                        color: Colors.orange,
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(height: 20.0),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 20,right: 20),
-                              child: _buildTextField('Login ID', Icons.person),
-                            ),
-                             Padding(
-                              padding: const EdgeInsets.only(left: 20,right: 20),
-                              child: _buildTextField('Password', Icons.lock, isPassword: false),
-                            ),
-                            const SizedBox(height: 20.0),
-                            Container(
-                              height: 56,
-                              width: double.infinity,
-                              padding: EdgeInsets.symmetric(horizontal: 16.0),
-                              child: ElevatedButton(
-                                onPressed: () {
-                                  // Add your sign-in logic here
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  primary: Colors.orange,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    side: BorderSide(color: Colors.orange),
-                                  ),
-                                ),
+                              Center(
                                 child: Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: Text(
-                                    'Sign In',
-                                    style: TextStyle(
-                                      fontSize: 18.0,
-                                      color: Colors.white,
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text: "Don't have an account yet ",
+                                          style: TextStyle(
+                                            color: Colors.grey,
+                                            fontSize: 16.0,
+                                          ),
+                                        ),
+                                        TextSpan(
+                                          text: "? Sign Up",
+                                          style: TextStyle(
+                                            color: Colors.orange,
+                                            fontSize: 16.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
                                     ),
                                   ),
                                 ),
                               ),
-                            )
-                            ,
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Center(
-                                child: Text(
-                                  'Forgot Password?',
-                                  style: TextStyle(
-                                    color: Colors.orange,
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            )
-                            ,
-                            // ... other widgets ...
-
-                            Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(16.0),
-                                child: RichText(
-                                  text: TextSpan(
-                                    children: [
-                                      TextSpan(
-                                        text: "Don't have an account yet ",
-                                        style: TextStyle(
-                                          color: Colors.grey,
-                                          fontSize: 16.0,
-                                        ),
-                                      ),
-                                      TextSpan(
-                                        text: "? Sign Up",
-                                        style: TextStyle(
-                                          color: Colors.orange,
-                                          fontSize: 16.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            )
-
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -153,19 +194,46 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildTextField(String label, IconData icon, {bool isPassword = false}) {
+  Widget _buildTextField(String label, IconData icon, TextEditingController controller,
+      {bool isPassword = false, bool isValid = true}) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
       child: TextField(
-        obscureText: isPassword,
+        controller: controller,
+        keyboardType: isPassword ? TextInputType.text : TextInputType.text,
+        obscureText: isPassword ? !isPasswordVisible : false,
         decoration: InputDecoration(
           labelText: label,
           prefixIcon: Icon(icon),
+          suffixIcon: isPassword
+              ? IconButton(
+            icon: Icon(
+              isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+              color: Colors.grey,
+            ),
+            onPressed: () {
+              setState(() {
+                isPasswordVisible = !isPasswordVisible;
+              });
+            },
+          )
+              : null,
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10.0),
+          ),
+          // Highlight the border if the field is not valid
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            borderSide: BorderSide(
+              color: isValid ? Colors.blue : Colors.red, // Change the border color for invalid fields
+            ),
           ),
         ),
       ),
     );
+  }
+
+  bool _isInputValid() {
+    return isPhoneNumberValid && isPasswordValid;
   }
 }
